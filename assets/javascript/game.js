e0de182be295011efb9f2e0e182049dacde320d6
stevenrobday <stevenrobday@gmail.com>
2018-07-01 21:35:36 -0700

    added finishing moves

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 function restart() {
     $("#opponentWrap").hide();
 
-    $(".darthVader").data("stats", { name: "Darth Vader", health: 100, attack: 3, counter: 6 });
-    $(".millenniumFalcon").data("stats", { name: "Millennium Falcon", health: 125, attack: 6, counter: 9 });
-    $(".maxRebo").data("stats", { name: "Max Rebo", health: 150, attack: 9, counter: 12 });
-    $(".jarJar").data("stats", { name: "Jar Jar Binks", health: 200, attack: 15, counter: 18 });
+    $(".darthVader").data("stats", { name: "Darth Vader", health: 100, attack: 3, attackPoints: 0, counter: 6, finishingMove: "Darth Vader lobs off [OPPONENT-POSSESSION] hand and proclaims to be [PRONOUN-POSSESSION] FATHER" });
+    $(".millenniumFalcon").data("stats", { name: "Millennium Falcon", health: 125, attack: 6, attackPoints: 0, counter: 9, finishingMove: "Millennium Falcon cleans [OPPONENT-POSSESSION] clock in less than 12 parsecs" });
+    $(".maxRebo").data("stats", { name: "Max Rebo", health: 150, attack: 9, attackPoints: 0, counter: 12, finishingMove: "Max Rebo uses jazz to enchant Obi Wan into slicing off [OPPONENT-POSSESSION] arm" });
+    $(".jarJar").data("stats", { name: "Jar Jar Binks", health: 200, attack: 15, attackPoints: 0, counter: 18, finishingMove: "Jar Jar Binks trips into [OPPONENT], knocking [PRONOUN] down a shaft inside the Death Star" });
 
     //$(".darthVader").data("stats").health = 69;
 
@@ -23,7 +23,7 @@ $(".players").on("click", function () {
 
     that.css("border-color", "#0506ec");
 
-    $(".players").each(function (i) {
+    $(".players").each(function () {
         if ($(this).attr("name") !== that.attr("name")) {
             $(this).hide();
         }
